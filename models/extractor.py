fixed tensor broadcasting bug in lstm crf

models/extractor.py
@@ -344,12 +344,12 @@ class LSTMCRFExtractor(Extractor):
         rationalevecs = []
 
         self.net.eval()
-        lstm_inp = torch.cat((bertops, attnscores), dim=2)
+        lstm_inp = torch.cat((bertops, attnscores.unsqueeze(2)), dim=2)
         scores, preds = self.net.tag_sequence(lstm_inp, mask=scoreop["tokenizer"]["attention_mask"]) # (batchsz, seqlen)
         for batch_item in range(0, bertops.shape[0]):
             i = 0
             while i < maxcontiglen[batch_item].item():
-                if i==1:
+                if preds[batch_item][i]==1:
                     j = i
                     while j < maxcontiglen[batch_item].item() and preds[batch_item][j]==1:
                         j += 1
@@ -432,10 +432,9 @@ def train_lstmcrf_model(train_x, dev_x, fsmodel, FILENAME, device, config):
 def load_extractor_model(model_file_path, featscorer):
     checkpoint = torch.load(model_file_path)
     model_name = checkpoint['config']['model_name']
-
     if model_name == 'heuristicext':
         return HeuristicExtractor(featscorer=featscorer, checkpoint=checkpoint)
-    if model_name == 'lstmcrf':
+    if model_name == 'lstm_crf':
         return LSTMCRFExtractor(featscorer=featscorer, checkpoint=checkpoint)
 
     del checkpoint

runnable.py
@@ -1,3 +1,4 @@
+import sys
 import pickle
 import torch
 import torch.nn as nn
@@ -14,7 +15,7 @@ class FeedbackModel:
         return rationale_data["rationales"]
 
 if __name__ == '__main__':
-    #fbmd = FeedbackModel()
-    #pickle.dump(fbmd, open('mymodel.pickle', 'wb'))
-    item = pickle.load(open('mymodel.pickle', 'rb'))
-    print(item.predict("In many settings it is important for one to be able to understand why a model made a particular prediction. In NLP this often entails extracting snippets of an input text responsible"))
+    inputstr = sys.argv[1]
+    fs = load_featurescorer_model('saved/suppmodeelnew.pt')
+    model = load_extractor_model('saved/lstmcrfmodel.pt', fs)
+    print(model.extract_rationales([inputstr])["rationales"])