5b6a08ea0907505d5ef4abcee365c97b92b129bd
aniruddhsriram <aniruddh.sriram@gmail.com>
2021-01-25 17:42:07 -0600

    startup script fix

--- startup.sh
+++ startup.sh
@@ -6,8 +6,6 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 pip --version
 git --version
-git clone https://github.com/AniruddhS24/FeedbackAnalysisSystem.git
-cd FeedbackAnalysisSystem
 pip install -r requirements.txt
 echo -e "Now upload data files, and saved model files from local machine or cloud"
 
